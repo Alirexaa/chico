85a0ac0e526295b714a289cf596133aa04939404
Alireza Baloochi <Dev.AlirezaBaloochi@gmail.com>
2025-03-14 19:06:47 +0330

    Refactor server to support multiple TCP listeners and improve connection handling

--- chico_server/src/server.rs
+++ chico_server/src/server.rs
@@ -2,23 +2,50 @@ use chico_file::types::Config;
 use http::{Request, Response};
 use hyper::{body::Body, server::conn::http1, service::service_fn};
 use hyper_util::rt::TokioIo;
-use std::{convert::Infallible, net::SocketAddr};
+use std::{convert::Infallible, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 
 use crate::handlers::{select_handler, BoxBody, RequestHandler};
 
 pub async fn run_server(config: Config) {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let ports = [3000];
 
-    let listener = match TcpListener::bind(addr).await {
-        Ok(listener) => listener,
-        Err(e) => {
-            eprintln!("Failed to bind to address {}: {:?}", addr, e);
-            return;
-        }
-    };
-    println!("Start listening requests on 3000");
+    let socket_addresses = ports.map(|port| SocketAddr::from(([127, 0, 0, 1], port)));
 
+    let mut listeners = vec![];
+
+    for addr in socket_addresses {
+        let listener = match TcpListener::bind(addr).await {
+            Ok(listener) => listener,
+            Err(e) => {
+                eprintln!("Failed to bind to address {}: {:?}", addr, e);
+                return;
+            }
+        };
+        listeners.push(listener);
+        println!(
+            "Start listening to incoming requests on port {}",
+            &addr.port()
+        );
+    }
+
+    let mut handles = vec![];
+
+    let config = Arc::new(config);
+
+    for listener in listeners {
+        let config_clone = config.clone();
+        let join_handle =
+            tokio::spawn(async move { handle_listener(config_clone, listener).await });
+        handles.push(join_handle);
+    }
+
+    for handle in handles {
+        let _ = handle.await; // Wait for each listener to complete
+    }
+}
+
+async fn handle_listener(config: Arc<Config>, listener: TcpListener) -> ! {
     loop {
         let (stream, _) = match listener.accept().await {
             Ok(conn) => conn,
@@ -27,29 +54,33 @@ pub async fn run_server(config: Config) {
                 continue;
             }
         };
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(stream);
-
-        // Spawn a tokio task to serve multiple connections concurrently
-        let config_clone = config.clone();
-
-        let service = service_fn(move |req| {
-            let config_clone = config_clone.clone();
-            async move { handle_request(req, config_clone).await }
-        });
-
-        tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service)
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-        });
+        handle_connection(&config, stream);
     }
+}
+
+fn handle_connection(config: &Config, stream: tokio::net::TcpStream) {
+    // Use an adapter to access something implementing `tokio::io` traits as if they implement
+    // `hyper::rt` IO traits.
+    let io = TokioIo::new(stream);
+
+    // Spawn a tokio task to serve multiple connections concurrently
+    let config_clone = config.clone();
+
+    let service = service_fn(move |req| {
+        let config_clone = config_clone.clone();
+        async move { handle_request(req, config_clone).await }
+    });
+
+    tokio::task::spawn(async move {
+        // Finally, we bind the incoming connection to our `hello` service
+        if let Err(err) = http1::Builder::new()
+            // `service_fn` converts our function in a `Service`
+            .serve_connection(io, service)
+            .await
+        {
+            eprintln!("Error serving connection: {:?}", err);
+        }
+    });
 }
 
 async fn handle_request(
