ff485da33035a992942b734f6e3bc8aa16f9a30d
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2025-07-11 22:02:38 +0330

    remove commented code

--- chico_server/src/server.rs
+++ chico_server/src/server.rs
@@ -131,13 +131,6 @@ async fn handle_request(
     Ok(response)
 }
 
-// async fn shutdown_signal() {
-//     // Wait for the CTRL+C signal
-//     tokio::signal::ctrl_c()
-//         .await
-//         .expect("failed to install CTRL+C signal handler");
-// }
-
 pub async fn shutdown_signal() {
     #[cfg(unix)]
     {
