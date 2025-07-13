925b85bb4a334a18f3ab6edc92905ae59225cac3
Alireza Baloochi <dev.alirezabaloochi@gmail.com>
2025-07-14 00:23:20 +0330

    lint

--- chico_server/src/load_balance/node.rs
+++ chico_server/src/load_balance/node.rs
@@ -26,6 +26,6 @@ impl FromStr for Node {
     type Err = AddrParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.parse::<SocketAddr>().and_then(|addr| Ok(addr.into()))
+        s.parse::<SocketAddr>().map(|addr| addr.into())
     }
 }
